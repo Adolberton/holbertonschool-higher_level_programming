5e8f6989249985af0c2e9872194b1d6e39c02ea9
Adolberton <7557@holbertonstudents.com>
2024-01-24 15:03:31 +0100

    nuw func

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,3 @@ def print_last_digit(number):
     l_digit = abs(number) % 10
     print(l_digit, end="")
     return (l_digit)
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
