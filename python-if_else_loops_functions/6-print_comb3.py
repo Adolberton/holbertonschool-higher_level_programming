eb333c08c979d6791c55338405687f92e2ed24af
Adolberton <7557@holbertonstudents.com>
2024-01-24 12:05:30 +0100

    new func

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@ for i in range(10):
     for j in range(10):
         if j > i:
             if i == 8 and j == 9:
-                print("{}{}".format(i, j), end="\n")
+                print("{}{}".format(i, j))
             else:
                 print("{}{}".format(i, j), end=", ")
