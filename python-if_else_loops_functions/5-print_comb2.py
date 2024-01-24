43a7e98d38ef4f54ecd969131bff08c444896c82
Adolberton <7557@holbertonstudents.com>
2024-01-24 10:38:19 +0100

    fix

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
-for i in range (100):
-    if i != 99:
-        print(f"{i}, ", end="")
-    else:
-        print(f"{i}")
+#!/usr/bin/python3
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
