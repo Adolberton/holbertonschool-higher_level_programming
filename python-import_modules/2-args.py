05af7807aecfa1ff15de1340fa2fc9381571a2fd
Adolberton <7557@holbertonstudents.com>
2024-01-25 14:44:36 +0100

    new func

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import sys 
+import sys
 if __name__ == "__main__":
     args = len(sys.argv) - 1
     if args == 0:
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         if args == 1:
             print("1 argument:")
         else:
-             print("{} arguments:".format(args))
+            print("{} arguments:".format(args))
         for i in range(1, args + 1):
-                print("{}: {}".format(i, sys.argv[i]))
+            print("{}: {}".format(i, sys.argv[i]))
