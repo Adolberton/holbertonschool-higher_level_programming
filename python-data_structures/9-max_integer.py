74a3d9647bff218216f37bc0d50421fdbabe433e
Adolberton <7557@holbertonstudents.com>
2024-01-29 15:51:05 +0100

    func

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,12 +4,6 @@ def max_integer(my_list=[]):
         return None
     max = 0
     for i in my_list:
-        if  i > max:
+        if i > max:
             max = i
     return max
-
-my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
-    
-    
