bab04e772930acfb20a83527ad522d6973fd2fdc
Adolberton <7557@holbertonstudents.com>
2024-01-29 12:36:10 +0100

    new func

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if i == "c" or i == "C":
-            i = ""
-
+    cless = ""
+    for i in range(len(my_string)):
+        if my_string[i] != "c" and my_string[i] != "C":
+            cless += my_string[i]
+    return cless
