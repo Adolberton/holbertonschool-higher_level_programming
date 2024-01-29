840519b48396affee1d701ee96ed21bfe297af59
Adolberton <7557@holbertonstudents.com>
2024-01-29 11:58:52 +0100

    new func

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,3 @@ def print_reversed_list_integer(my_list=[]):
     reversed_list = reversed(my_list)
     for i in reversed_list:
         print(i)
-
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integer(my_list)
