92ebb01415daf2a7879eaef066b5ecc157c6352e
Adolberton <adonisriahi@gmail.com>
2024-01-30 11:48:20 +0100

    rework

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
-    def square(nb):
-        return nb ** 2
-    for line in matrix:
-        new_matrix += list(map(square, line))
+    new_matrix = [list(map(lambda x: x ** 2, line)) for line in matrix]
     return new_matrix
