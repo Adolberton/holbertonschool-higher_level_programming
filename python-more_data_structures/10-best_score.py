b887c1d55c3ce141b41d102270cb48ed62cd5409
Adolberton <7557@holbertonstudents.com>
2024-02-01 14:06:54 +0100

    auto commit

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if  not key in a_dictionary:
+    if key not in a_dictionary:
         return None
     best_key = None
     best_value = 0
@@ -8,4 +8,4 @@ def best_score(a_dictionary):
         if value > best_value:
             best_value = value
             best_key = best_value
-    return best_value
+    return best_key
