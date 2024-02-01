1cff66b51b71f0977f6d6d60a43736eaaea610c0
Adolberton <7557@holbertonstudents.com>
2024-02-01 14:16:59 +0100

    auto commit

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary: dict):
     sorted_keys = sorted(a_dictionary.keys())
-
+    
     for key in sorted_keys:
-        value = a_dictionary[key]
-        print(f"{key}: {value}")
+        print(f"{key}: {a_dictionary[key]}")
