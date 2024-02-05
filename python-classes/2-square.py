e495b7a6f8f020bfd4ffdee28ecc2a717c851c1a
Adolberton <7557@holbertonstudents.com>
2024-02-05 12:01:51 +0100

    auto commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""this script write a class Square that defines a square """
+
+
+class Square:
+    """this class defines a square"""
+    def __init__(self, size=0):
+        """init new square instance"""
+        try:
+            self.__size = size
+        except TypeError("size must be an integer"):
+            pass
+        except ValueError("size must be >= 0"):
+            pass
