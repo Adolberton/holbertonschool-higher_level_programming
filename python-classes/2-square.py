c3e95c6f92d4ec471e5c012c787689901b8dd531
Adolberton <7557@holbertonstudents.com>
2024-02-05 12:08:34 +0100

    auto commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,9 +6,8 @@ class Square:
     """this class defines a square"""
     def __init__(self, size=0):
         """init new square instance"""
-        try:
-            self.__size = size
-        except TypeError("size must be an integer"):
-            pass
-        except ValueError("size must be >= 0"):
-            pass
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
