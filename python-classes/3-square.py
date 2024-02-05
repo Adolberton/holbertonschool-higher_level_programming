d71110e9acfe11d648a1af08a2b7e8c8128086be
Adolberton <7557@holbertonstudents.com>
2024-02-05 13:40:42 +0100

    auto commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,7 +11,5 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """init new square instance"""
-        pass
+        def area(self):
+            pass
