7dab143f2489a7c54a898c39ffc571c71e00592f
Adolberton <7557@holbertonstudents.com>
2024-02-05 11:40:17 +0100

    auto commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""this script write a class Square that defines a square """
+
+
+class Square:
+    """this class defines a square"""
+    def __init__(self, size):
+        """init new square instance"""
+        self.__size = size
