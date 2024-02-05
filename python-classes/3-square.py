47f123249b271ac62150756aade0f9752ec810bf
Adolberton <7557@holbertonstudents.com>
2024-02-05 13:31:13 +0100

    auto commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""this script write a class Square that defines a square """
+
+
+class Square:
+    """this class defines a square"""
+    def area(self):
+        """init new square instance"""
+    def __init__(self, size=0):
+        """init new square instance"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
