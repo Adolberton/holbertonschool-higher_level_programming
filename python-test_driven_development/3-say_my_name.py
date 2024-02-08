5c01a2b250161a36661dac6b6b8037c50bfe60f6
Adolberton <7557@holbertonstudents.com>
2024-02-08 14:23:41 +0100

    auto commit

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""heisenberg"""
+
+
 def say_my_name(first_name, last_name=""):
+    """i am the danger"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
