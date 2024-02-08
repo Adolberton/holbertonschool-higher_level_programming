28b3309488399537feb29f66b0f138beec8c4739
Adolberton <7557@holbertonstudents.com>
2024-02-08 16:11:30 +0100

    auto commit

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""text indent module"""
+
+
+def text_indentation(text):
+    """indentation text function"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    in_sentence = False
+
+    for char in text:
+        if char in ('.', '?', ':'):
+            in_sentence = False
+            print(char, end="")
+            print('\n')
+        elif char == ' ' and not in_sentence:
+            continue
+        else:
+            in_sentence = True
+            print(char, end="")
