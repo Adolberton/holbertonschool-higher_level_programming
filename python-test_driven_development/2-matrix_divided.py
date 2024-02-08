a84300248ddb9c8f87564a54144a09041851dc7b
Adolberton <7557@holbertonstudents.com>
2024-02-08 14:33:41 +0100

    auto commit

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,10 +5,11 @@
 def matrix_divided(matrix, div):
     """Divide all elements of a matrix by a given divisor (`div`)."""
     e = "matrix must be a matrix (list of lists) of integers/floats"
+
     def err(str):
         """raise the error"""
         if str == "list":
-            raise TypeError(f"{e}")
+            raise TypeError(e)
         if str == "row":
             raise TypeError("Each row of the matrix must have the same size")
         if str == "div":
@@ -20,7 +21,7 @@ def matrix_divided(matrix, div):
         err("list")
     ref_size = len(matrix[0])
     for row in matrix:
-        if not isinstance(row, list)
+        if not isinstance(row, list):
             err("list")
         if len(row) != ref_size:
             err("row")
@@ -34,13 +35,3 @@ def matrix_divided(matrix, div):
 
     matrix_divided = [[round((nb / div), 2) for nb in row] for row in matrix]
     return matrix_divided
-
-matrix = [
-    [-3.14159, -2.71828, -1.41421],
-    [6.28318, -5.43656, -2.82843],
-    [-9.42477, -8.15484, -4.24264]
-]
-
-
-print("Matrice d'origine :", matrix)
-print("Matrice après opération :", matrix_divided(matrix, -2))
