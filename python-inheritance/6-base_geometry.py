bd1df63bbd66ac884be305599f2ebc77b8da512d
Adolberton <7557@holbertonstudents.com>
2024-02-14 15:04:35 +0100

    auto commit

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+"""
+Module with an empty class `BaseGeometry`.
+"""
+
+
+class BaseGeometry:
+    """
+    Defines the BaseGeometry class.
+    """
+    def area(self):
+       raise Exception("area() is not implemented")
