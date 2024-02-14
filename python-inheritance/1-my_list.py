a8195941dee6a2eeefa68d3feba953f699649d69
Adolberton <7557@holbertonstudents.com>
2024-02-14 14:40:38 +0100

    auto commit

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""classe list commentaire"""
+
+
+class MyList(list):
+    """classe list commentaire"""
+    def __init__(self):
+        pass
+    def print_sorted(self):
+        print(sorted(self))
