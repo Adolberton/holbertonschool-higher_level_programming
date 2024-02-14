8ebd592e2a77d1d0b1f78f03434b2fe5e3d36f8f
Adolberton <7557@holbertonstudents.com>
2024-02-14 14:50:25 +0100

    auto commit

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,8 +4,5 @@
 
 def is_same_class(obj, a_class):
     """classe list commentaire"""
-
-    if issubclass(obj, a_class):
-        return True
-    else:
-        return False
+    
+    return(type(obj) is a_class)
