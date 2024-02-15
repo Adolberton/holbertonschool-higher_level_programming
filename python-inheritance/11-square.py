b0805c2e953d4dca1c862c0162ff6c446755b713
Adolberton <7557@holbertonstudents.com>
2024-02-15 11:54:32 +0100

    auto commit

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
-"""classe list commentaire"""
 
+"""
+This module defines 'the Square` class and the `Rectangle` class.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Initializes a new  instance."""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
