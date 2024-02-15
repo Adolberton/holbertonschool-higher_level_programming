5831cf32aed4320687a6b01142e8e4c6348ea916
Adolberton <7557@holbertonstudents.com>
2024-02-15 15:34:01 +0100

    auto commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""holberton mandatory doc"""
+
 
 import sys
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 def add_item(args):
+    """holberton mandatory doc"""
     filename = "add_item.json"
     list = []
     for _ in args[1:]:
         list += _
     save_to_json_file(list, filename)
-
