87ab632e30b11410d739ff848110e1c7f50ae326
Adolberton <7557@holbertonstudents.com>
2024-02-15 15:37:24 +0100

    auto commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,7 +11,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 def add_item(args):
     """holberton mandatory doc"""
     filename = "add_item.json"
-    list = []
-    for _ in args[1:]:
-        list += _
+    list = load_from_json_file(filename)
+    list.extend(args[1:])
+
     save_to_json_file(list, filename)
