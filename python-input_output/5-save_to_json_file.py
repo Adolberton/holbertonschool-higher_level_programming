b05c8513789b934073314df4757a2872581e7587
Adolberton <7557@holbertonstudents.com>
2024-02-15 14:58:58 +0100

    auto commit

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""holberton mandatory doc"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """holberton mandatory doc"""
+    return json.dump(my_obj, filename)
