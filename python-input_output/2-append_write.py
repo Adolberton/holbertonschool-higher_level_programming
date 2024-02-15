4a737406396239fc7507b144b83c61f8539b69d4
Adolberton <7557@holbertonstudents.com>
2024-02-15 14:44:27 +0100

    auto commit

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """holberton mandatory doc"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """holberton mandatory doc"""
     with open(filename, 'a', encoding='utf-8') as fd:
         bytes_written = fd.write(text)
