6319f4977f888e196a91b87f5fc6a9264b8d8ca5
Adolberton <7557@holbertonstudents.com>
2024-02-15 15:32:55 +0100

    auto commit

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+def add_item(args):
+    filename = "add_item.json"
+    list = []
+    for _ in args[1:]:
+        list += _
+    save_to_json_file(list, filename)
+
