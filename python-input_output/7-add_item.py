8277b8e9dc588781c634e39aaaebf5c1705bd4e2
Adolberton <7557@holbertonstudents.com>
2024-02-15 16:12:03 +0100

    auto commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,14 +3,16 @@
 
 
 import sys
-import json
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = "add_item.json"
-argument = sys.argv[1:]
-list = load_from_json_file(filename)
-list.extend(argument)
+if not list:
+    list = []
+else:
+    list = load_from_json_file(filename)
+list.extend(sys.argv[1:])
 save_to_json_file(list, filename)
