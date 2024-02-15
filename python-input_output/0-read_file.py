1251ef5e25deb06cd786bf13dbdcad378fbdd903
Adolberton <7557@holbertonstudents.com>
2024-02-15 14:11:31 +0100

    auto commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """holberton mandatory doc"""
-    with open(filename, 'r') as fd:
+    with open(filename, 'r', encoding='utf-8') as fd:
         text = fd.read()
         print(text)
