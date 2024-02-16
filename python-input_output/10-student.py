1d7e02c40e03af1a02cfb7b3d0da1cb4c763ddbc
Adolberton <7557@holbertonstudents.com>
2024-02-16 12:30:47 +0100

    auto commit

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""holberton mandatory doc"""
+
+
+class Student:
+    """holberton mandatory doc"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            new_dict = {}
+            for key in attrs:
+                new_dict[key] = self.__dict__[key]
+            return new_dict
+        else:
+            return self.__dict__
