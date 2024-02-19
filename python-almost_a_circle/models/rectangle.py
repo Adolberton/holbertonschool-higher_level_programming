1b6050b9b9e72d78d932ab129769b2166f96b32d
Adolberton <7557@holbertonstudents.com>
2024-02-19 12:11:24 +0100

    auto commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,41 @@
 #!/usr/bin/python3
 """Holberton mandatory doc"""
 
+from base import Base
 
+
+class Rectangle(Base):
+    """Holberton mandatory doc"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+    
+    @property
+    def width(self):
+        return self.__width
+    @property
+    def height(self):
+        return self.__height
+    @property
+    def x(self):
+        return self.__x
+    @property
+    def y(self):
+        return self.__y
+    
+    @width.setter
+    def width(self):
+        pass
+    @height.setter
+    def height(self):
+        pass
+    @x.setter
+    def x(self):
+        pass
+    @y.setter
+    def y(self):
+        pass
