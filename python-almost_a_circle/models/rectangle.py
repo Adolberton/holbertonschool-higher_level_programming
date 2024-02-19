633661106ba150b38f1f56fc54ac87010adc82ae
Adolberton <7557@holbertonstudents.com>
2024-02-19 14:55:29 +0100

    auto commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -70,8 +70,9 @@ class Rectangle(Base):
 
     def display(self):
         """x must be an integer more"""
+        print(self.y * "\n")
         for line in range(self.height):
-            print(self.width * "#")
+            print((self.x * " ") + (self.width * "#"))
 
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} -\
