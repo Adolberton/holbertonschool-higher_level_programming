ebf7d100fdb31192cf2712c9c8dcd353f2fb91ee
Adolberton <7557@holbertonstudents.com>
2024-02-19 11:49:14 +0100

    auto commit

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Holberton mandatory doc"""
+
+class Base:
+    """Holberton mandatory doc"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
