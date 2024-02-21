f92a2ce556dba9ed54d4694451ea020f534e8723
Adolberton <7557@holbertonstudents.com>
2024-02-21 15:03:28 +0100

    auto commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """dumps list"""
         if list_dictionaries is None:
             return "[]"
         return dumps(list_dictionaries)
