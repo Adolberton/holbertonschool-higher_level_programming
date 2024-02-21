ffa7e8969a5669011f7449d099b879792638b4b3
Adolberton <7557@holbertonstudents.com>
2024-02-21 14:30:28 +0100

    auto commit

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -39,5 +39,4 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """jimmy neutron"""
-        return {'x': self.x, 'y': self.y, 'id': self.id,
-"size": self.size }
+        return {'x': self.x, 'y': self.y, 'id': self.id, "size": self.size}
