6af5343316094e984662201ce69320f7ff138642
Adolberton <7557@holbertonstudents.com>
2024-02-21 15:03:01 +0100

    auto commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Holberton mandatory doc"""
 
+from json import dumps
+
 
 class Base:
     """Holberton mandatory doc"""
@@ -12,3 +14,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        return dumps(list_dictionaries)
