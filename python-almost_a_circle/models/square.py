b7c9595bfbbc5934548c4f685228ef6680528367
Adolberton <adonisriahi@gmail.com>
2024-02-21 11:14:30 +0100

    auto commit

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Holberton mandatory doc"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
